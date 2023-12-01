0c79e947aae60c9785ae19eea8f93cd5fcb5671b
nateturk <nateturk42@gmail.com>
2023-12-01 13:21:45 -0800

    big test pusherson

---
+++ main.py
@@ -0,0 +1,9 @@
+# This file was created by Nate Turkington
+
+##### SOURCES #####
+# help + basecode from https://www.youtube.com/watch?v=y2UzjhnJN_k&list=PLhTjy8cBISEo3SzET7Fc3-b4miKWp41yX&index=13 
+# content from kids can code: http://kidscancode.org/blog/
+# help from geeks for geeks: https://www.geeksforgeeks.org/
+# help from https://www.youtube.com/watch?v=gnlWy-qi6Rs&list=PLhTjy8cBISEo3SzET7Fc3-b4miKWp41yX&index=4
+# help from https://www.youtube.com/@buildwithpython "tutorial"
+
